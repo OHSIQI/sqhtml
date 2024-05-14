a5e6c22fa17d46f983d0ca10389108c6fa905703
OHSIQI <167868342+OHSIQI@users.noreply.github.com>
2024-05-14 04:30:20 +0000

    Java Handson

---
+++ script.js
@@ -0,0 +1,35 @@
+var age = 20
+
+var username = "monster"
+var password = "eatup"
+
+var isMale = false
+var isLoggedIn = false
+
+var blog_posts = ["Vegatables are good you're eating too much", "Meat", "Seafood"]
+
+console.log(age)
+console.log(username);
+console.log(password);
+console.log(isMale);
+console.log(isLoggedIn);
+console.log(blog_posts);
+
+if (isMale == true) {
+    console.log("Hey Man!")
+} 
+
+if (isMale == true) {
+    console.log("Hey Man!")
+} else {
+    console.log("Hey Superwoman!")
+}
+
+while (age < 25) {
+    console.log("Your age is " + age );
+    age = age + 1;
+}
+
+function sayHello() {
+    alert("Hello everyone");
+}
